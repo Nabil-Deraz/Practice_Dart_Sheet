be458ecc7da78405a22680a93fa9364ccb07cc45
Nabil Mohamed <109727309+Nabil-Deraz@users.noreply.github.com>
2023-09-13 03:03:11 +0300

    task 13
    Write a program (function) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.

--- main.dart
+++ main.dart
@@ -1,32 +1,39 @@
 import 'dart:io';
-
 void main() {
-  print("please enter a number to generate the sequance");
-  int num = int.parse(stdin.readLineSync()!);
-  List<int> fib_list = fibonacci(num);
-  print('The first $num Fibonacci numbers are: $fib_list');
+  List inputList =[];
+  print('Enter Size of list');
+  int size = int.parse(stdin.readLineSync()!);
+  for (int i =0; i<size;i++){
+  int? input;
+  print("enter numbers");
+  do{
+
+    input = int.parse(stdin.readLineSync()??"");
+    
+  
+  
+}
+while(input==null);
+
+  inputList.add(input);
+  
+}
+  
+  
+  List newList = removeDuplicates(inputList);
+  
+  print('Original List: $inputList');
+  print('List without duplicates: $newList');
 }
 
-
-List<int> fibonacci(int num) {
-  if (num <= 0) {
-    print("Please enter a number above 0");
+List removeDuplicates(List inputList) {
+  List unduplicatedList = [];
+  
+  for (var item in inputList) {
+    if (!unduplicatedList.contains(item)) {
+      unduplicatedList.add(item);
+    }
   }
-
-  List<int> fib_seq = [1, 1];
-
-  if (num == 1) {
-    return [1];
-  } else if (num == 2) {
-    return fib_seq;
-  }
-
-  for (int i = 2; i < num; i++) {
-    int sum = fib_seq[i - 1] + fib_seq[i - 2];
-    fib_seq.add(sum);
-  }
-
-  return fib_seq;
+  
+  return unduplicatedList;
 }
-
-
